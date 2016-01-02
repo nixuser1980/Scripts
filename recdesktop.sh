7992bbfbf3979f597d6c33089d7e37a760d4eaf3
nixuser1980 <nixuser1980@gmail.com>
2016-01-02 22:34:36 +0800

    added recdesktop.sh for recording desktop using ffmpeg/avconv

---
+++ recdesktop.sh
@@ -0,0 +1,31 @@
+#Screen recording script by Damien Sticklen
+
+#!/bin/sh
+#avconv -video_size 1920x1080 -f x11grab -i :0.0 output.mp4
+
+FILEPREFIX=screencast
+EXTENSION=.mkv
+NUM=0
+FILENAME=$FILEPREFIX$NUM$EXTENSION
+EXECPATH=/usr/bin
+
+while [ -f $FILENAME ]; do
+	NUM=$((NUM + 1))
+	FILENAME=$FILEPREFIX$NUM$EXTENSION
+done
+
+if [ -f $EXECPATH/ffmpeg ]; then
+	EXEC=$EXECPATH/ffmpeg
+elif [ -f $EXECPATH/avconv ]; then
+	EXEC=$EXECPATH/avconv
+else
+	echo "No ffmpeg/avconv found in $EXECPATH. Install ffmpeg/avconv or modify EXECPATH in this script to point to the install path."
+	exit 999
+fi
+
+PARAMETERS="-video_size 1920x1080 -r 60 -f x11grab -i :0.0 -f alsa -i hw:0,0 -codec:v libx264 -crf 0 -preset ultrafast -codec:a flac $FILENAME"
+	
+echo -e $EXEC $PARAMETERS '\n'
+$EXEC -video_size 1920x1080 -r 60 -f x11grab -i :0.0 -f alsa -i hw:0,0 -codec:v libx264 -crf 0 -preset ultrafast -codec:a flac $FILENAME
+
+echo -e "\nFile saved as $PWD/$FILENAME"
